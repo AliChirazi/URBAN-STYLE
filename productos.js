6145f8aa279df1a7b4b16e56147ba64fdc5963f3
AliChirazi <aliciachirazi@gmail.com>
2025-09-17 16:17:12 -0300

    Agregué productos con imágenes y mejoré el CSS

---
+++ productos.js
@@ -0,0 +1,74 @@
+const productos =[
+    {id: "camisero-01",
+     titulo:"camisero beige",
+     precio:"$34499",
+     categoria:"Vestidos",
+     imagen: "img/camisero.png"
+    },
+    {id: "camisero-02",
+     titulo:"camisero azul",
+     precio:"$34499",
+     categoria:"Vestidos",
+     imagen: "img/camiseroAzul.png"
+    },
+     {id: "pollera-01",
+     titulo:"Falda cargo",
+     precio:"$40000",
+     categoria:"polleras",
+     imagen: "img/cargoBeige.png"
+    },
+     {id: "pollera-02",
+     titulo:"Falda jean blanca",
+     precio:"$45000",
+     categoria:"polleras",
+     imagen: "img/jeanBlanca.png"
+    },
+     {id: "pollera-03",
+     titulo:"falda jean celeste",
+     precio:"$45000",
+     categoria:"polleras",
+     imagen: "img/jeanCeleste.png"
+    },
+     {id: "piletero-01",
+     titulo:"piletero azul",
+     precio:"$$42000",
+     categoria:"Pileteros",
+     imagen: "img/pileteroAzul.png"
+    },
+     {id: "piletero-02",
+     titulo:"piletero verde",
+     precio:"$$42000",
+     categoria:"Vestidpileterosos",
+     imagen: "img/pileteroVerde.png"
+    },
+     {id: "vestido-01",
+     titulo:"vestido gris",
+     precio:"$30000",
+     categoria:"Vestidos",
+     imagen: "img/vestidoGris.png"
+    },
+    {id: "vestido-02",
+     titulo:"vestido negro",
+     precio:"$30000",
+     categoria:"Vestidos",
+     imagen: "img/vestidoNegro.png"
+    },
+    {id: "remera-01",
+     titulo:"remera morley gris",
+     precio:"$20000",
+     categoria:"remeras",
+     imagen: "img/remeraGris.png"
+    },
+     {id: "remera-02",
+     titulo:"remera morley negra",
+     precio:"$20000",
+     categoria:"remeras",
+     imagen: "img/remeraNegra.png"
+    },
+     {id: "remera-03",
+     titulo:"remera morley verde",
+     precio:"$20000",
+     categoria:"remeras",
+     imagen: "img/remeraVerde.png"
+    },
+]
